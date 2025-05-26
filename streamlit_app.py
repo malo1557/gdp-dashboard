c363f0e34f32a7cf9c3df7cdde6e0d87e00bca4c
malo1557 <168790964+malo1557@users.noreply.github.com>
2025-05-26 02:32:22 +0000

    이름수정

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,38 @@
+
 import streamlit as st
 
-st.title("안녕하세요")
-st.write("이규빈 입니다")
-st.write("반갑습니다.99")
+# --- 사이드바 (슬라이더) ---
+st.sidebar.title("사이드바")
+slider_val = st.sidebar.slider("값 선택", 0,10,5)
+
+# --- 탭 구성 ---
+tab1, tab2, tab3 = st.tabs(["탭 01", "탭 02", "탭 03"])
+
+with tab1:
+    st.write("이규빈")
+
+    # 2x2 레이아웃 구성
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown("### 🧱 레이아웃 01")
+        st.info(f"슬라이더 값: {slider_val}")
+    with col2:
+        st.markdown("### 🧱 레이아웃 02")
+        st.success("오른쪽 상단 영역")
+
+    col3, col4 = st.columns(2)
+    with col3:
+        st.markdown("### 🧱 레이아웃 03")
+        st.warning("왼쪽 하단 영역")
+    with col4:
+        st.markdown("### 🧱 레이아웃 04")
+        st.error("오른쪽 하단 영역")
+
+with tab2:
+    st.write("탭 02 내용")
+    
+
+with tab3:
+    st.write("탭 03 내용")
+
+
